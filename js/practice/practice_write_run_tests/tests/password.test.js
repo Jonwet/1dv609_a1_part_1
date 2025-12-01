12e5528ca53d953d1034bb3d71f3c576a53f8c2c
Jonatan <jw223tb@student.lnu.se>
2025-12-01 11:36:35 +0100

    Add tests for DoesNotHash, DoesNotTrim and isPasswordAlwaysSame

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,8 +1,7 @@
-
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
-// import { Password } from '../src/BugDoesNotTrim'
+// import { Password } from "../src/BugDoesNotHash";
+// import { Password } from "../src/BugDoesNotTrim";
 // import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
 // import { Password } from '../src/BugMissingPasswordCheck'
@@ -11,15 +10,33 @@ import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugVeryShort'
 // import { Password } from '../src/BugWrongHashingAlgorithm'
 // import { Password } from '../src/BugWrongMessage'
-// import { Password } from '../src/Correct'
+// import { Password } from "../src/Correct";
 
-describe('Password class, test suite', () => {
-    //put constants here to increase readability
-    const emptyPassword = '';
+describe("Password class, test suite", () => {
+  //put constants here to increase readability
+  const testPassword = "Jonatan123456789";
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
-    });
+test("Passwords can be different", () => {
+  const password = new Password("Jonatan123456789")
+  const passwordReversed = new Password("987654321natanoJ")
 
-    //Add your tests here
+  expect(password.isPasswordSame(passwordReversed)).toBe(false)
+})
+
+  test("Constructor should trim password", () => {
+    const passwordWithSpaces = new Password("  Jonatan123456789  ");
+    const passwordWithoutSpaces = new Password("Jonatan123456789");
+
+    expect(passwordWithSpaces.isPasswordSame(passwordWithoutSpaces)).toBe(true)
+  });
+
+  test("Constructor should hash password", () => {
+    const password = new Password(testPassword);
+
+    let hash = 7;
+    for (let i = 0; i < testPassword.length; i++) {
+      hash = hash * 31 + testPassword.charCodeAt(i);
+    }
+    expect(password.getPasswordHash()).toBe(hash);
+  });
 });
